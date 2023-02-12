c15b2bd5ff867cbc1998d5aa46fd1f8a22e01671
SergeyShavurov <115174346+SergeyShavurov@users.noreply.github.com>
2023-02-12 23:36:54 +0300

    Lab_Works_3 (#3)

---
+++ LabWork_3/task1/main.py
@@ -0,0 +1,69 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self.name = name
+        self.author = author
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, name: str) -> None:
+        self._name = name
+
+    @property
+    def author(self) -> str:
+        return self._author
+
+    @author.setter
+    def author(self, author: str) -> None:
+        self._author = author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages
+
+    @property
+    def pages(self) -> int:
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages: int) -> None:
+        if pages < 1:
+            raise ValueError("Книга не может содержать менее 1-й страницы")
+        if not isinstance(pages, int):
+            raise TypeError("Количество страниц может быть только целым числом (int значение)")
+        self._pages = pages
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration
+
+    @property
+    def duration(self) -> float:
+        return self._duration
+
+    @duration.setter
+    def duration(self, duration: float) -> None:
+        if duration <= 0:
+            raise ValueError("Продолжительность не может быть менее нуля")
+        if not isinstance(duration, float):
+            raise TypeError("Продолжительность книги может быть только числом")
+        self._duration = duration
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
